b3bf27bf350102dd4b7ccecdd144349d537c3d4b
kmvishnu <kmvishnu1996@gmail.com>
2023-12-22 13:52:32 +0530

    removed the package mysql2

--- src/common/config.ts
+++ src/common/config.ts
@@ -11,7 +11,7 @@ export module Config {
 
 	export const MySqlDB = new Sequelize(process.env.MYSQL_DB_NAME, process.env.MYSQL_DB_USER, process.env.MYSQL_DB_PASS, {
 		host: process.env.MYSQL_DB_HOST,
-		dialect: 'mysql',
+		dialect: 'mssql', //actually mssql
 		logging: false
 	});	
 	
